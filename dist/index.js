11fd49f23e005603138827fe79195777840c4ea1
rbrogi <roberto.brogi@beije.it>
2021-10-29 15:33:17 +0200

    add dist file

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Counter", {
+  enumerable: true,
+  get: function get() {
+    return _Counter.default;
+  }
+});
+
+var _Counter = _interopRequireDefault(require("./Counter"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
